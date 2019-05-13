10144ef70f63e265de6517232a859938a808a509
hao wang <ge_nah@hotmail.com>
2019-05-13 20:26:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,7 +48,9 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   supplys = []
   holiday_hash[:winter].each do |holiday, supply|
-    supplys << supply
+    supply.each do |word|
+      supplys << word
+    end
   end
   supplys
 end
