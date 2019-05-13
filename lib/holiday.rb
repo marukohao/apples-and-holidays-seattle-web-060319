865c43d22705c035ecd08104d64bf843ad54bd28
hao wang <ge_nah@hotmail.com>
2019-05-13 20:53:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts season.to_s.capitalize + ":"
     holiday.each do |key, value|
-      array = key.to_s.split(" ")
+      array = key.to_s.split("_")
       puts "  " + array.map {|word| word.capitalize}.join(" ") + ": " + value.join(", ")
     end
   end
