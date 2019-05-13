edac74b9c8135dbeb12d2213aa7f14679dc18ee1
hao wang <ge_nah@hotmail.com>
2019-05-13 20:38:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,7 +77,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  array - []
+  array = []
   holiday_hash.each do |season, holiday|
     holiday.each do |key, value|
       if value.include?("BBQ")
